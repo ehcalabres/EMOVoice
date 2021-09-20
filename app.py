65071f8dbe71fee76a66c5fb8f9a162894831fea
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2021-09-20 16:45:50 +0200

    Added initial streamlit application (WIP)

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.sidebar.title('EMOVoice')
+st.sidebar.write('Welcome to EMOVoice, a tool for Speech Emotion Recognition based on the Wav2Vec2 model.')
+
+st.title('EMOVoice')
+
+st.write("This is a work in progress, stay tuned!")

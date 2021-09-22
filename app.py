29317689f5c4b9059863224b69f7ccc99c912356
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2021-09-22 02:13:35 +0200

    Added request to EMOVoice model and prediction logging

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import json
 import os
+import requests
+import json
 import streamlit as st
 
 EXAMPLE_PATH = []
@@ -19,12 +22,32 @@ file = None
 
 if input_mode == 'Upload audio':
     file = st.sidebar.file_uploader("Choose a file", type=['mp3', 'mp4', 'wav', 'flac'])
+    file_size = file.size if file else None
 elif input_mode == 'Select example':
     example_selected = st.sidebar.selectbox('Choose an audio example', EXAMPLE_PATH)
     file = open('data/' + example_selected, 'rb')
+    file_size = os.stat('data/' + example_selected).st_size
 
 if file is not None:
     st.write('Audio added!')
     audio_bytes = file.read()
     st.audio(audio_bytes)
+
+
+    url = "https://api-inference.huggingface.co/models/ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition"
+
+    payload=file
+    headers = {
+        'Content-Type': 'audio/mp3',
+        'Authorization': 'Bearer ' + st.secrets['API_TOKEN']
+    }
+
+    response = requests.request("POST", url, headers=headers, data=audio_bytes)
+
+    response.request
+
+    decoded_response = json.loads(response.text)
+    st.write(decoded_response)
+
+
     file.close()
